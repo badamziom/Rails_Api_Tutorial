a1b6d0b7bb74c12d11562013cc2d3fabcfb0cb2f
Adam <aszulist@o2.pl>
2016-06-12 08:34:57 +0200

    Adds update action the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -40,15 +40,51 @@ describe Api::V1::UsersController do
       before(:each) do
         post :create, { user: invalid_user_attributes }, format: :json
       end
+      let(:user_response) { JSON.parse(response.body, symbolize_names: true) }
 
       it 'should renders an error json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response).to have_key(:errors)
       end
 
       it 'should renders the json errors on why the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it { should respond_with 422 }
+
+    end
+  end
+
+  describe 'PUT/PATCH #update' do
+
+    context 'when is successfully updated' do
+      let(:user) { FactoryGirl.create(:user) }
+      before(:each) do
+        patch :update, { id: user.id, user: { email: 'newmail@example.com' } }, format: :json
+      end
+      let(:user_response) { JSON.parse(response.body, symbolize_names: true) }
+
+      it 'should renders the json representation for the updated user' do
+        expect(user_response[:email]).to eql('newmail@example.com')
+      end
+
+      it { should respond_with 200 }
+
+    end
+
+    context 'when is not created' do
+      let(:user) { FactoryGirl.create(:user) }
+      before(:each) do
+        patch :update, { id: user.id, user: { email: 'badmail.com' } }, format: :json
+      end
+      let(:user_response) { JSON.parse(response.body, symbolize_names: true) }
+
+      it 'should renders an errors json' do
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'should renders the json errors on while the user could notbe created' do
+        expect(user_response[:errors][:email]).to include('is invalid')
       end
 
       it { should respond_with 422 }
