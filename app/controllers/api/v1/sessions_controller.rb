f0c1bc3783c93382e99304a951b173827318d4a0
Adam <aszulist@o2.pl>
2016-06-13 08:12:54 +0200

    Adds destory session action added

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user_password = params[:session][:password]
     user_email = params[:session][:email]
-    user = user_email.present? && User.find_by(email: user_email)
+    user = user_email.present? && User.find_by_email(user_email)
 
     if user.valid_password? user_password
       sign_in user, store: false
@@ -13,7 +13,13 @@ class Api::V1::SessionsController < ApplicationController
     else
       render json: { errors: "Invalid email or password" }, status: 422
     end
+  end
 
+  def destroy
+    user = User.find_by_auth_token(params[:id])
+    user.generate_authentication_token!
+    user.save
+    head 204
   end
 
 end
