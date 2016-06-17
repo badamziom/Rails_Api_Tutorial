f1b74f253ed6068f3687a28eebbc05bcfdeab5c3
Adam <aszulist@o2.pl>
2016-06-17 07:07:12 +0200

    Allows the order to be placedalong with product quantity|

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,4 +13,12 @@ class Order < ActiveRecord::Base
     self.total = products.map(&:price).sum
   end
 
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantitites)
+    product_ids_and_quantitites.each do |product_id_and_quantity|
+      id, quantity = product_id_and_quantity
+
+      self.placements.build(product_id: id)
+    end
+  end
+
 end
